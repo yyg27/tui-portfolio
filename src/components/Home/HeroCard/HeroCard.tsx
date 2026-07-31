0db502514ae0114e1582f4ed8fbfa284e3dcbda1
yyg27 <yyg27@outlook.com>
2026-07-31 21:20:28 +0300

    refactor:change folder, file names to HeroCard from AboutCard

---
+++ src/components/Home/HeroCard/HeroCard.tsx
@@ -0,0 +1,9 @@
+import styles from "./AboutCard.module.css";
+
+export default function AboutCard() {
+    return (
+        <section className={styles.aboutCard}>
+            About Card
+        </section>
+    );
+}
