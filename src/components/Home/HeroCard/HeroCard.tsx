ef0e3cf2483dcc85089bd40cebc8baeb18c6ce25
yyg27 <yyg27@outlook.com>
2026-08-02 23:06:04 +0300

    feat: add HeroCard component and social data

--- src/components/Home/HeroCard/HeroCard.tsx
+++ src/components/Home/HeroCard/HeroCard.tsx
@@ -1,9 +1,42 @@
 import styles from "./HeroCard.module.css";
+import { socialLinks } from "../../../data/social.data";
 
 export default function HeroCard() {
     return (
         <section className={styles.heroCard}>
-            Hero Card
+            <div className={styles.portrait}>
+                <pre>
+{`
+   /\\_/\\\\
+  ( o.o )
+   > ^ <
+`}
+                </pre>
+            </div>
+
+            <div className={styles.content}>
+                <div className={styles.intro}>
+                    <h1>Yiğit Gültekin</h1>
+
+                    <p>
+                        Computer Engineering Student passionate about Linux,
+                        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    </p>
+                </div>
+
+                <div className={styles.links}>
+                    {socialLinks.map((link) => (
+                        <a
+                            key={link.label}
+                            href={link.href}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            [{link.short}] {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
         </section>
     );
 }
---
+++ src/data/social.data.ts
@@ -0,0 +1,22 @@
+export const socialLinks = [
+    {
+        short: "G",
+        label: "GitHub",
+        href: "https://github.com/yyg27",
+    },
+    {
+        short: "L",
+        label: "LinkedIn",
+        href: "#",
+    },
+    {
+        short: "M",
+        label: "Mail",
+        href: "mailto:example@example.com",
+    },
+    {
+        short: "C",
+        label: "CV",
+        href: "#",
+    },
+];
