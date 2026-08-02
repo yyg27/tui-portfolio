14176a7fcdf4071e66ffc1138a69b2d9f844ee8f
yyg27 <yyg27@outlook.com>
2026-08-02 23:19:39 +0300

    feat: apply some colors to work better on ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/variables.css'
+import './styles/globals.css'
+import './styles/typography.css'
 import './index.css'
 import App from './App.tsx'
 
